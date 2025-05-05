5ef5b7e7715b691c95a8217b0a3ed4aecd63ddd6
chandu <chandu.kpudi@gmail.com>
2025-05-05 11:43:47 +1000

    create csv and save

--- arpi.py
+++ arpi.py
@@ -11,6 +11,6 @@ with open('students.csv', mode='w', newline='', encoding='utf-8') as file:
     writer.writerow([106052, 'srikar', 'srikar@gmail.com', 2025, 'Arts', 'Bachelor', 'Online', 25000])
     writer.writerow([106053, 'srinika', 'srinika@hotmail.com', 2025, 'Law', 'Diploma', 'Oncampus', 15000])
     writer.writerow([106054, 'riyansh', 'riyansh@outlook.com', 2025, 'Cybersecurity', 'Certificate', 'Oncampus', 8000])
-    writer.writerow([106055, 'rishik', 'rishik@outlook.com', 2025, 'Business', 'Diploma', 'Online', 10000])
+    writer.writerow([106055, 'rishik', 'rishik@outlook.com', 2025, 'Arts', 'Diploma', 'Online', 10000])
     writer.writerow([106056, 'murali', 'murali@gmail.com', 2025, 'Animation', 'Advanced Diploma', 'Oncampus', 20000])
     writer.writerow([106057, 'deepu', 'deepu@outlook.com', 2025, 'Health', 'Master', 'Online', 25000])
