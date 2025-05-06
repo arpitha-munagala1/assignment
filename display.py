40aa99b9c2d5b9ecf0a9139aa1bea2b5d1c3c0cb
Arpitha <arpitha.munagala1@outlook.com>
2025-05-06 14:42:11 +1000

    Load Records

---
+++ display.py
@@ -0,0 +1,28 @@
+def display_records():
+    if not student_ids:
+        print("\nNo records to display. Please load or add records first.\n")
+        return
+
+    print(
+        f"{'ID':>8}  {'Name':<10}  {'Email':<25}  {'Year':>6}  {'Course':<15}  {'Qual.':<18}  {'Mode':<10}  {'Fees':>10}")
+    print("-" * 110)
+    for i in range(len(student_ids)):
+        print(f"{student_ids[i]:>8}  {names[i]:<10}  {emails[i]:<25}  {years[i]:>6}  "
+              f"{courses[i]:<15}  {qualifications[i]:<18}  {study_modes[i]:<10}  {fees[i]:>10}")
+    print()
+
+def add_record():
+    print("\nEnter new student record:")
+    student_ids.append(input("Student ID: "))
+    names.append(input("Name: "))
+    emails.append(input("Email: "))
+    years.append(input("Year: "))
+    courses.append(input("Course: "))
+    qualifications.append(input("Qualification: "))
+    study_modes.append(input("Study Mode (Online/Oncampus): "))
+    fees.append(input("Fees: "))
+    print("\n Record added successfully.\n")
+
+    # Display all record after successfullyy adding files
+    print("\n current Records:")
+    display_records()
